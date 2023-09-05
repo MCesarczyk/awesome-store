3daa8b701ef7db5c874427bd516aa16b091be0b3
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-09-05 20:29:34 +0200

    test: add testid to list

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -7,7 +7,10 @@ interface ProductListProps {
 
 export const ProductList = ({ products }: ProductListProps) => {
 	return (
-		<ul className="grid grid-cols-1 gap-6 sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+		<ul
+			className="grid grid-cols-1 gap-6 sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+			data-testid="products-list"
+		>
 			{products.map((product) => (
 				<ProductListItem key={product.id} product={product} />
 			))}
