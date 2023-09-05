4875983b589cd2cc6e54b39ac639c2d75bbbe1e9
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-09-06 00:53:16 +0200

    refactor: move products under home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,12 @@
-import { ProductList } from "@/ui/organisms/ProductList";
-import type { ProductItem } from "@/ui/types";
-
-const products: ProductItem[] = [
-	{
-		id: 1,
-		category: "milling",
-		name: "shell mill",
-		price: 200,
-		image: {
-			src: "/shell_mill.jpeg",
-			alt: "shell mill",
-		},
-	},
-	{
-		id: 2,
-		category: "milling",
-		name: "porcupine mill",
-		price: 200,
-		image: {
-			src: "/porcupine_mill.jpeg",
-			alt: "porcupine mill",
-		},
-	},
-	{
-		id: 3,
-		category: "milling",
-		name: "form mill",
-		price: 200,
-		image: {
-			src: "/form_mill.jpeg",
-			alt: "form mill",
-		},
-	},
-	{
-		id: 4,
-		category: "milling",
-		name: "maxi mill",
-		price: 200,
-		image: {
-			src: "/maxi_mill.jpeg",
-			alt: "maxi mill",
-		},
-	},
-];
+import Link from "next/link";
 
 export default function Home() {
 	return (
-		<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-2">
-			<ProductList products={products} />
-		</section>
+		<main className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-2">
+			<h1>CNC TOOL STORE</h1>
+			<Link href="/products">
+				<h2>Products</h2>
+			</Link>
+		</main>
 	);
 }
