d457c17baa0ac69cb26082fc01c40dfdf1c4210b
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-09-12 07:18:21 +0200

    style(pagination): update look

--- src/ui/organisms/Pagination.tsx
+++ src/ui/organisms/Pagination.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export const Pagination = () => {
 	return (
-		<label className="flex flex-nowrap justify-center gap-4 py-6 text-2xl font-bold text-white">
+		<label className="flex flex-nowrap justify-center gap-4 py-6 text-2xl font-bold text-slate-600">
 			pagination
 			<Link
 				href={`/products?page=1`}
-				className={`text-2xl font-bold text-white hover:text-slate-500`}
+				className={`text-2xl font-bold text-pink-600 hover:text-pink-300`}
 			>
 				1
 			</Link>
