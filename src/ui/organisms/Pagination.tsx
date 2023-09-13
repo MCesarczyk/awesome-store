015af9d534982b4debc3bc02f82719e05d692923
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-09-13 07:37:46 +0200

    feat(pagination): add page links based on total results number

--- src/ui/organisms/Pagination.tsx
+++ src/ui/organisms/Pagination.tsx
@@ -1,15 +1,35 @@
 import Link from "next/link";
+import { productsApi } from "@/api/poductsApi";
 
-export const Pagination = () => {
+interface PaginationProps {
+	page?: number;
+	perPage?: number;
+}
+
+export const Pagination = async ({ page = 1, perPage = 10 }: PaginationProps) => {
+	const products = await productsApi.getProducts();
+
+	const pages = Math.ceil(products.length / perPage);
+
+	if (!pages) {
+		return null;
+	}
+
 	return (
-		<label className="flex flex-nowrap justify-center gap-4 py-6 text-2xl font-bold text-slate-600">
-			pagination
-			<Link
-				href={`/products?page=1`}
-				className={`text-2xl font-bold text-pink-600 hover:text-pink-300`}
-			>
-				1
-			</Link>
+		<label
+			className="flex flex-nowrap justify-center gap-4 py-6 text-2xl font-bold text-slate-600"
+			data-testid="pagination"
+		>
+			pagination {page}/{products.length}
+			{[...Array(pages).keys()].map((page, index) => (
+				<Link
+					key={page}
+					href={`/products?page=${index + 1}`}
+					className={`text-2xl font-bold text-pink-600 hover:text-pink-300`}
+				>
+					{index + 1}
+				</Link>
+			))}
 		</label>
 	);
 };
