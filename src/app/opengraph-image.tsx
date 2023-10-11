15ee7e4c6525df077b0806d2b00b8db0c3216149
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2023-10-11 17:57:01 +0200

    feat: add opengraph image

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,29 @@
+import { ImageResponse } from "next/server";
+ 
+export const runtime = "edge";
+ 
+export const alt = "awesome shop";
+export const content = "Awesome shop";
+export const size = {
+	width: 1280,
+	height: 640,
+};
+ 
+export const contentType = "image/png";
+ 
+// eslint-disable-next-line import/no-default-export
+export default async function og() {
+	return new ImageResponse(
+		(
+			<div
+				tw="w-full text-white h-full flex flex-col items-center justify-center text-8xl"
+				style={{
+					background: '#525252',
+				}}
+			>
+				<p tw="font-sans uppercase m-0 p-0 text-[64px] leading-4">Awesome shop</p>
+				<p tw="font-serif mt-20 p-0 font-black">Use on your own risk!</p>
+			</div>
+		),
+	);
+}
