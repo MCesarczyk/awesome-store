f4ec59cd69306a9e345635bf7b99a1dddc51c6bf
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2023-10-22 11:40:17 +0200

    fix(product): update type in story

--- src/ui/atoms/productDescription/ProductDescription.stories.tsx
+++ src/ui/atoms/productDescription/ProductDescription.stories.tsx
@@ -28,5 +28,7 @@ Default.args = {
     description: 'A basic t-shirt for everyone.',
     categories: [{id:'1', name:'Accessories',slug:'accessories',description:''}],
     price: 29,
+    colors: [{id:'1', name:'Black', value:'#000'},{id:'2', name:'White', value:'#fff'}],
+    sizes: [{id:'1', name:'S', value: 1},{id:'2', name:'M', value: 2},{id:'3', name:'L', value: 3},{id:'4', name:'XL', value: 4}],
   },
 };
