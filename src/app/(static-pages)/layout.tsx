0c930cdae17de0baa2491e98a7a35838d8f8dd11
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-04-13 02:48:08 +0200

    feat(mdx): fine tune, use prose style from tailwind typography

--- src/app/(static-pages)/layout.tsx
+++ src/app/(static-pages)/layout.tsx
@@ -1,6 +1,6 @@
 export default function StaticLayout({children}: {children: React.ReactNode}) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-white">
       <main className="p-8">{children}</main>
     </div>
   );
